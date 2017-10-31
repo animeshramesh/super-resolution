466112b8699e65df4493d8bf9a0432b01fe85993
Animesh <animeshramesh@gmail.com>
2017-10-30 22:04:15 -0400

    Generating patches

--- generate_training_data.py
+++ generate_training_data.py
@@ -6,17 +6,75 @@ This script will generate the corresponding low-res images in dataset/input.
 '''
 
 import os
-import numpy
+import numpy as np
 import scipy
 import scipy.misc, scipy.ndimage
+from scipy.misc import imsave, imread, imresize
+from scipy.ndimage.filters import gaussian_filter
+import tqdm
+import time
 
+# Paths
+DATASET_DIR = '/Users/admin/Downloads/images_all/'
+TARGET_DIR = '/Users/admin/Dev/super-resolution/dataset/'
 
-INPUT_DIR = 'dataset/output'
-OUTPUT_DIR = 'dataset/input'
-DOWNSAMPLE_RATIO = 0.25
+# Constants
+SCALE_FACTOR = 2
+IMG_SHAPE = 256          # must be divisible by stride
+STRIDE = 16
+LR_PATCH_SIZE = 32
+HR_PATCH_SIZE = LR_PATCH_SIZE * SCALE_FACTOR
 
-for img_file in os.listdir(INPUT_DIR):
-    img = scipy.misc.imread(os.path.join(INPUT_DIR, img_file))
-    blurred = scipy.ndimage.filters.gaussian_filter(img, sigma=(3, 3, 0))
-    downsampled_img = scipy.misc.imresize(blurred, DOWNSAMPLE_RATIO)
-    scipy.misc.imsave(os.path.join(OUTPUT_DIR, img_file), downsampled_img)
+# Create target directory if not present
+if not os.path.exists(TARGET_DIR):
+    os.makedirs(TARGET_DIR)
+if not os.path.exists(os.path.join(TARGET_DIR, 'Y')):
+    os.makedirs(os.path.join(TARGET_DIR, 'Y'))
+if not os.path.exists(os.path.join(TARGET_DIR, 'X')):
+    os.makedirs(os.path.join(TARGET_DIR, 'X'))
+
+def subimage_generator(img, stride, patch_size, nb_hr_images):
+    for _ in range(nb_hr_images):
+        for x in range(0, IMG_SHAPE - patch_size, stride):
+            for y in range(0, IMG_SHAPE - patch_size, stride):
+                subimage = img[x : x + patch_size, y : y + patch_size, :]
+
+                yield subimage
+
+for index, img_file in enumerate(os.listdir(DATASET_DIR)):
+    img = imread(os.path.join(DATASET_DIR, img_file), mode='RGB')
+
+    # Resize to 256 x 256
+    img = imresize(img, (IMG_SHAPE, IMG_SHAPE))
+
+     # Create patches
+    nb_hr_images = (IMG_SHAPE ** 2) // (STRIDE ** 2)    # Flooring division
+    hr_samples = np.empty((nb_hr_images, HR_PATCH_SIZE, HR_PATCH_SIZE, 3))
+    image_subsample_iterator = subimage_generator(img, STRIDE, HR_PATCH_SIZE, nb_hr_images)
+
+    stride_range = np.sqrt(nb_hr_images).astype(int)
+
+    i = 0
+    for j in range(stride_range):
+        for k in range(stride_range):
+            hr_samples[i, :, :, :] = next(image_subsample_iterator)
+            i += 1
+
+    t1 = time.time()
+    # Create nb_hr_images 'X' and 'Y' sub-images of size hr_patch_size for each patch
+    for i in range(nb_hr_images):
+        ip = hr_samples[i]
+
+        # Save ground truth image Y
+        imsave(TARGET_DIR + "/Y/" + "%d_%d.png" % (index + 1, i + 1), ip)
+
+        # Apply Gaussian Blur to Y
+        op = gaussian_filter(ip, sigma=0.5)
+
+        # Subsample by scaling factor to create X
+        op = imresize(op, (LR_PATCH_SIZE, LR_PATCH_SIZE), interp='bicubic')
+
+        # Save X
+        imsave(TARGET_DIR + "/X/" + "%d_%d.png" % (index + 1, i+1), op)
+
+    print("Finished image %d in time %0.2f seconds.. " % (index + 1, time.time() - t1))
